2435055cdc2bf0284a0cf228ecfcb17de4cc6a2c
James Fredley <jamesfredley@users.noreply.github.com>
2025-10-18 18:55:40 -0400

    Add simple render and remove duplicate

--- grails-app/controllers/com/example/ContestController.groovy
+++ grails-app/controllers/com/example/ContestController.groovy
@@ -4,9 +4,13 @@ class ContestController {
 
     def index() { }
 
-    def dq(long id, long imageId) {}  // WORKS FINE!
-    def submit(long id, long images) {} // GroovyCastException: Cannot cast object '1000311' with class 'java.lang.String' to class 'long'
-    def prepareQuestionnaire(Long id) {}  // GroovyCastException: Cannot cast object '1000271' with class 'java.lang.String' to class 'java.lang.Long'
-
-    def submit(long images) { params.long('id')}
+    def dq(long id, long imageId) {
+        render "id: $id, imageId: $imageId"
+    }  // WORKS FINE!
+    def submit(long id, long images) {
+        render "id: $id, images: $images"
+    } // GroovyCastException: Cannot cast object '1000311' with class 'java.lang.String' to class 'long'
+    def prepareQuestionnaire(Long id) {
+        render "id: $id"
+    }  // GroovyCastException: Cannot cast object '1000271' with class 'java.lang.String' to class 'java.lang.Long'
 }
